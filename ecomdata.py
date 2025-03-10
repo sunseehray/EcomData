d080ebb2fe460d1ddda3903b93d0c565aa6d6a96
sunseehray <sabtirazona@byui.edu>
2025-03-10 12:07:56 +0800

    notes on how to go with analysis

---
+++ ecomdata.py
@@ -0,0 +1,14 @@
+# Ecom Data Analysis
+# https://www.kaggle.com/datasets/cemeraan/fecom-inc-e-com-marketplace-orders-data-crm
+
+# Questions to answer:
+# Which country has the biggest sales for the period?
+# Which gender has higher average spending in France?
+
+# Aggregate information by country as sum of sales, then get the country with the highest sum
+# Aggregate information by gender and filter by country, France then sum sales by gender, compare
+
+# Stretch challenge
+# try graphing if time permits, if not, answer third question:
+# Which MONTH generated lowest sales?
+# Aggregate information by month, sum sales, then get the one with the lowest value.

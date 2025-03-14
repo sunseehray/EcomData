96e4d34324f15370e37fb160043e0cdf4c7a4d51
sunseehray <sabtirazona@byui.edu>
2025-03-14 17:53:51 +0800

    general plan and tutorial links

--- ecomdata.py
+++ ecomdata.py
@@ -12,3 +12,25 @@
 # try graphing if time permits, if not, answer third question:
 # Which MONTH generated lowest sales?
 # Aggregate information by month, sum sales, then get the one with the lowest value.
+
+print("Hello world!")
+
+# How do you extract data from file?
+# https://pandas.pydata.org/docs/user_guide/10min.html#importing-and-exporting-data
+
+# QUESTION 1
+# Group by country - how to group: https://pandas.pydata.org/docs/user_guide/10min.html#grouping
+# Getting sum based on filters: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.sum.html
+
+# QUESTION 2
+# Aggregate by country 
+# Filter France
+# Aggregate by gender
+# Sum? or does aggregate get the sum automatically?
+
+# STRETCH QUESTION 3
+# Filter sales by year
+# Aggregate by month
+# Find MAX value
+
+# Try graphing

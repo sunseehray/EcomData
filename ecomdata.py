370b59f74b40ce3b40bb9c4b398efed0b6440e1e
sunseehray <sabtirazona@byui.edu>
2025-03-15 21:17:45 +0800

    sales by country done

--- ecomdata.py
+++ ecomdata.py
@@ -26,7 +26,13 @@ print("Hello world!")
 # https://pandas.pydata.org/docs/user_guide/10min.html#importing-and-exporting-data
 
 path_customers = "ecomdataset/Fecom Inc Customer List.csv"
-customers = pd.read_csv("ecomdataset/Fecom Inc Customer List.csv", sep=";")
+customers = pd.read_csv(path_customers, sep=";")
+
+path_orders = "ecomdataset/Fecom Inc Orders.csv"
+orders = pd.read_csv(path_orders, sep=";")
+
+path_order_payments = "ecomdataset/Fecom Inc Order Payments.csv"
+payments = pd.read_csv(path_order_payments, sep=";")
 
 # QUESTION 1
 # Group by country - how to group: https://pandas.pydata.org/docs/user_guide/10min.html#grouping
@@ -35,7 +41,24 @@ customers = pd.read_csv("ecomdataset/Fecom Inc Customer List.csv", sep=";")
 # To get orders by country, I need customer information
 # From the customer information, I can get the Customer_Country
 
+# Figure out how to merge there three data frames so I can get a list of orders by country and payment value !!!!!!
+
+# merge orders and customer to see which country customer is from
+orders_country = pd.merge(orders, customers, on="Customer_Trx_ID", how="inner")
+
+# merge orders_country to see payment_value for each order
+orders_country_payments = pd.merge(orders_country, payments, on="Order_ID", how="inner")
+
+# sales by country -- I DID IT!!!!
+country_sales = orders_country_payments.groupby("Customer_Country")[["Payment_Value"]].sum()
+print(country_sales)
+
+
+
+
+
 countries = customers["Customer_Country"].dropna().unique()
+print("Num of countries:")
 print(len(countries))
 
 # 27 countries
@@ -43,6 +66,17 @@ print(len(countries))
 #  'Czechia' 'Belgium' 'Greece' 'Switzerland' 'United Kingdom' 'Austria'
 #  'Norway' 'Portugal' 'Turkey' 'Luxembourg' 'Slovakia' 'Serbia' 'Slovenia'
 #  'Latvia' 'Lithuania' 'Denmark' 'Croatia' 'Estonia' 'Andorra' 'Finland']
+
+# get from Orders list of orders where year is 2023
+orders_2023 = orders[orders["Order_Approved_At"].astype(str).str[:4] == "2023"].shape[0]
+
+print("2023 orders: ")
+# 44973
+print(orders_2023)
+
+print("All orders:")
+# 99441
+print(len(orders))
 
 # QUESTION 2
 # Aggregate by country 

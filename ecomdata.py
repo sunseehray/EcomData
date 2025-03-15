36681cff559adb0d3b452d34d81124ee8aecb5d7
sunseehray <sabtirazona@byui.edu>
2025-03-15 14:00:14 +0800

    kaggle data

--- ecomdata.py
+++ ecomdata.py
@@ -1,5 +1,7 @@
 # Ecom Data Analysis
-# https://www.kaggle.com/datasets/cemeraan/fecom-inc-e-com-marketplace-orders-data-crm
+
+import numbers as np
+import pandas as pd
 
 # Questions to answer:
 # Which country has the biggest sales for the period?
@@ -15,12 +17,32 @@
 
 print("Hello world!")
 
-# How do you extract data from file?
+
+# GET DATA
+# Data source:
+# https://www.kaggle.com/datasets/cemeraan/fecom-inc-e-com-marketplace-orders-data-crm
+
+# Extract data from file
 # https://pandas.pydata.org/docs/user_guide/10min.html#importing-and-exporting-data
+
+path_customers = "ecomdataset/Fecom Inc Customer List.csv"
+customers = pd.read_csv("ecomdataset/Fecom Inc Customer List.csv", sep=";")
 
 # QUESTION 1
 # Group by country - how to group: https://pandas.pydata.org/docs/user_guide/10min.html#grouping
 # Getting sum based on filters: https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.sum.html
+
+# To get orders by country, I need customer information
+# From the customer information, I can get the Customer_Country
+
+countries = customers["Customer_Country"].dropna().unique()
+print(len(countries))
+
+# 27 countries
+# ['France' 'Poland' 'Netherlands' 'Italy' 'Sweden' 'Spain' 'Germany'
+#  'Czechia' 'Belgium' 'Greece' 'Switzerland' 'United Kingdom' 'Austria'
+#  'Norway' 'Portugal' 'Turkey' 'Luxembourg' 'Slovakia' 'Serbia' 'Slovenia'
+#  'Latvia' 'Lithuania' 'Denmark' 'Croatia' 'Estonia' 'Andorra' 'Finland']
 
 # QUESTION 2
 # Aggregate by country 
